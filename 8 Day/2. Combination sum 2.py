7a6e25abd1fc128b52ed1c629e5037907485781b
Boppa Sri Satya Sai Hruday <81548274+20481A1219@users.noreply.github.com>
2023-06-20 20:50:45 +0530

    Update 2. Combination sum 2.py

--- 8 Day/2. Combination sum 2.py
+++ 8 Day/2. Combination sum 2.py
@@ -8,8 +8,8 @@ class Solution:
                 return
             for j in range(i,len(c)):
                 if j>i and c[j]==c[j-1]:        # After sorting as we are not permitted to take same element more than once we should give if present element is same as previous element
-                    continue
-                if c[j]>target:
+                    continue                    # That means it is a same level call so see at 10:45 of striver video so we dont need to take the same element again and form a new recursioin
+                if c[j]>target:                
                     break
                 ds.append(c[j])
                 find(ans,ds,target-c[j],j+1)
