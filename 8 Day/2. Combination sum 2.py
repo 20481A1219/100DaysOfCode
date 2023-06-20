304675ec8a1af8fafaeeec979c278d4e250d0de6
Boppa Sri Satya Sai Hruday <81548274+20481A1219@users.noreply.github.com>
2023-06-20 20:37:14 +0530

    Create 2. Combination sum 2.py

---
+++ 8 Day/2. Combination sum 2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum2(self, c: List[int], target: int) -> List[List[int]]:
+        ans=[]
+        c.sort()
+        def find(ans,ds,target,i):
+            if target==0:
+                ans.append(ds.copy())
+                return
+            for j in range(i,len(c)):
+                if j>i and c[j]==c[j-1]:        # After sorting as we are not permitted to take same element more than once we should give if present element is same as previous element
+                    continue
+                if c[j]>target:
+                    break
+                ds.append(c[j])
+                find(ans,ds,target-c[j],j+1)
+                ds.pop()
+        find(ans,[],target,0)
+        return ans
+
+       """
+       TC - O(2^N * K)
+       SC - N*K
+       """
