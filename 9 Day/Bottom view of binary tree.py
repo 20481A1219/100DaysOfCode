b6de9ee3f4609600899f096d5d1ea7c1d3c51ff6
Boppa Sri Satya Sai Hruday <81548274+20481A1219@users.noreply.github.com>
2023-06-25 23:12:18 +0530

    Create Bottom view of binary tree.py

---
+++ 9 Day/Bottom view of binary tree.py
@@ -0,0 +1,26 @@
+class Solution:
+    def bottomView(self, root):
+        # code here
+        if not root:
+            return []
+        q=deque([(root,0)])
+        d=defaultdict(int)
+        while q:
+            k,l=q.popleft()
+            d[l]=k.data
+            if(k.left):
+                q.append((k.left,l-1))
+            if(k.right):
+                q.append((k.right,l+1))
+        ans=[]
+        for i in sorted(d.keys()):
+            ans.append(d[i])
+        return ans
+
+# Take verticals line in each node 
+# And for every vertical line take only the last node of it and add it to the map.
+
+  """
+  TC-O(N)
+  SC-O(N)
+  """
